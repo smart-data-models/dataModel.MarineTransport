239022fcc2aca4d200e1304eb3f4ac490817fa16
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:42:49 +0200

    created example of code

--- EdiCodeco/code/code_for_using_dataModel.MarineTransport_EdiCodeco.py
+++ EdiCodeco/code/code_for_using_dataModel.MarineTransport_EdiCodeco.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "EdiCodeco"
 subject = "dataModel.MarineTransport"
-fileName = "{'type': 'Property', 'value': 'file name'}"
+fileName = "file name"
 attribute = "fileName"
 value = fileName
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-sentAt = "{'type': 'Property', 'value': {'@value': '2023-08-11T14:50:00Z', '@type': 'DateTime'}}"
+sentAt = "2023-08-11T14:50:00Z"
 attribute = "sentAt"
 value = sentAt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-travelReference = "{'type': 'Property', 'value': '110823110823CCHRIB'}"
+travelReference = "110823110823CCHRIB"
 attribute = "travelReference"
 value = travelReference
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-ata = "{'type': 'Property', 'value': {'@value': '2023-08-11T14:47:00Z', '@type': 'DateTime'}}"
+ata = "2023-08-11T14:47:00Z"
 attribute = "ata"
 value = ata
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
