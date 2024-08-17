a5ad90674b41953dfd7b415758a76bbd84b7de4a
Alberto Abella <alberto.abella@fiware.org>
2024-08-18 01:13:42 +0200

    Draft version of pydantic file v2

---
+++ Booking/code/code_for_using_pydantic.py
@@ -0,0 +1,214 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import (
+    AnyUrl,
+    AwareDatetime,
+    BaseModel,
+    Field,
+    RootModel,
+    confloat,
+    constr,
+)
+
+
+class Address(BaseModel):
+    addressCountry: Optional[str] = Field(
+        None, description='The country. For example, Spain'
+    )
+    addressLocality: Optional[str] = Field(
+        None,
+        description='The locality in which the street address is, and which is in the region',
+    )
+    addressRegion: Optional[str] = Field(
+        None,
+        description='The region in which the locality is, and which is in the country',
+    )
+    district: Optional[str] = Field(
+        None,
+        description='A district is a type of administrative division that, in some countries, is managed by the local government',
+    )
+    postOfficeBoxNumber: Optional[str] = Field(
+        None,
+        description='The post office box number for PO box addresses. For example, 03578',
+    )
+    postalCode: Optional[str] = Field(
+        None, description='The postal code. For example, 24004'
+    )
+    streetAddress: Optional[str] = Field(None, description='The street address')
+    streetNr: Optional[str] = Field(
+        None, description='Number identifying a specific property on a public street'
+    )
+
+
+class BookingStatus(Enum):
+    Pending = 'Pending'
+    No_show = 'No show'
+    Visited = 'Visited'
+    Cancelled_by_user__on_time_ = 'Cancelled by user (on time)'
+    No_slot_booking = 'No-slot booking'
+
+
+class Type(Enum):
+    Point = 'Point'
+
+
+class Location(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[float] = Field(..., min_length=2)
+    type: Type
+
+
+class Coordinate(RootModel[List[float]]):
+    root: List[float]
+
+
+class Type1(Enum):
+    LineString = 'LineString'
+
+
+class Location1(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[Coordinate] = Field(..., min_length=2)
+    type: Type1
+
+
+class Type2(Enum):
+    Polygon = 'Polygon'
+
+
+class Location2(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type2
+
+
+class Type3(Enum):
+    MultiPoint = 'MultiPoint'
+
+
+class Location3(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[float]]
+    type: Type3
+
+
+class Type4(Enum):
+    MultiLineString = 'MultiLineString'
+
+
+class Location4(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type4
+
+
+class Type5(Enum):
+    MultiPolygon = 'MultiPolygon'
+
+
+class Location5(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[List[Coordinate]]]
+    type: Type5
+
+
+class Booking(BaseModel):
+    actualWindowFrom: Optional[float] = Field(
+        None, description='Time window actually used, if arrival was 15’ earlier'
+    )
+    actualWindowTo: Optional[float] = Field(
+        None, description='Time window actually used'
+    )
+    address: Optional[Address] = Field(None, description='The mailing address')
+    alternateName: Optional[str] = Field(
+        None, description='An alternative name for this item'
+    )
+    areaServed: Optional[str] = Field(
+        None,
+        description='The geographic area where a service or offered item is provided',
+    )
+    bookingDate: Optional[float] = Field(None, description='Booking date')
+    bookingNumber: Optional[float] = Field(None, description='Unique ID of the booking')
+    bookingStatus: Optional[BookingStatus] = Field(None, description='Booking status')
+    company: Optional[str] = Field(None, description='Company making the booking')
+    containersExport: Optional[confloat(ge=0.0, le=2.0)] = Field(
+        None, description='Number of containers to pick-up from the CT yard'
+    )
+    containersImport: Optional[confloat(ge=0.0, le=2.0)] = Field(
+        None, description='Number of containers to drop-off to the CT yard'
+    )
+    dataProvider: Optional[str] = Field(
+        None,
+        description='A sequence of characters identifying the provider of the harmonised data entity',
+    )
+    dateCreated: Optional[AwareDatetime] = Field(
+        None,
+        description='Entity creation timestamp. This will usually be allocated by the storage platform',
+    )
+    dateModified: Optional[AwareDatetime] = Field(
+        None,
+        description='Timestamp of the last modification of the entity. This will usually be allocated by the storage platform',
+    )
+    description: Optional[str] = Field(None, description='A description of this item')
+    enterDate: Optional[float] = Field(None, description='Actual time of entering')
+    exportContainer1: Optional[str] = Field(
+        None, description='Unique container ID for 1st container to be picked-up'
+    )
+    exportContainer2: Optional[str] = Field(
+        None, description='Unique container ID for 2nd container to be picked-up'
+    )
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    importContainer1: Optional[str] = Field(
+        None, description='Unique container ID for 1st container to be dropped-off'
+    )
+    importContainer2: Optional[str] = Field(
+        None, description='Unique container ID for 2nd container to be dropped-off'
+    )
+    location: Optional[
+        Union[Location, Location1, Location2, Location3, Location4, Location5]
+    ] = Field(
+        None,
+        description='Geojson reference to the item. It can be Point, LineString, Polygon, MultiPoint, MultiLineString or MultiPolygon',
+    )
+    name: Optional[str] = Field(None, description='The name of this item')
+    originalWindowFrom: Optional[float] = Field(
+        None, description='Originally booked time window to enter'
+    )
+    originalWindowTo: Optional[float] = Field(
+        None, description='Originally estimated time window to leave'
+    )
+    owner: Optional[
+        List[
+            Union[
+                constr(
+                    pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!,:\\\\]+$',
+                    min_length=1,
+                    max_length=256,
+                ),
+                AnyUrl,
+            ]
+        ]
+    ] = Field(
+        None,
+        description='A List containing a JSON encoded sequence of characters referencing the unique Ids of the owner(s)',
+    )
+    seeAlso: Optional[Union[List[AnyUrl], AnyUrl]] = Field(
+        None, description='list of uri pointing to additional resources about the item'
+    )
+    source: Optional[str] = Field(
+        None,
+        description='A sequence of characters giving the original source of the entity data as a URL. Recommended to be the fully qualified domain name of the source provider, or the URL to the source object',
+    )
+    windowDate: Optional[float] = Field(None, description='Date of the time window')
